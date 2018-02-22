db640095eb290479d717221a7213414b7bca40bf
DanielH4 <36571081+DanielH4@users.noreply.github.com>
2018-02-22 23:54:56 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 
-import IphoneLayout from './Components/IphoneLayout'
-
 import './App.css';
 
 //Main class. Keep all components separate in the components folder.
 class App extends Component {
+
+    state = {
+        studentID: "StudentID"
+    }
+
+    switchID(value) {
+        this.setState({ studentID: value })
+    }
+
+
     render() {
         return (
             //All components go inside this div.
             <div className="App">
-                <IphoneLayout className="Iphone" />
+                <div className="IphoneLayout" />
+                <div className="HeaderSection">
+                    <h1 className="HeaderWeather">QMULweather</h1>
+                    <h4 className="StudentID">{this.state.studentID}</h4>
+                    <button className="ChangeButton" onClick={(e) => { this.switchID("test"); }}>Change</button>
+                </div>
             </div>
         );
     }
